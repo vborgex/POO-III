a5f45f7db991bd2ee7f8247289ee3ab90f63e272
vborgex <vanessa_borges123@outlook.com>
2024-06-20 21:29:48 -0300

    Agr ta commitado

--- TrabalhoDecorator/src/components/CafeComponent.ts
+++ TrabalhoDecorator/src/components/CafeComponent.ts
@@ -1,7 +1,7 @@
 import IComponent from "./IComponent";
 
 //Cafe component fornece implementação padrão da operation. Pode haver diversas variações dessa classe.
-export default class CafeComponent {
+export default class CafeComponent implements IComponent{
     public operation(): string {
       return 'Café simples';
     }
